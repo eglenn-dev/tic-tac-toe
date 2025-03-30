6aee5099832a927efa34698aa5b94feead17cbed
Ethan Glenn <ethan+git@eglenn.dev>
2025-03-30 13:23:42 -0600

    Add Watermark component to display project attribution

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import TicTacToe from "./components/tic-tac-toe";
+import Watermark from "./components/watermark";
 
 function App() {
     return (
         <main>
             <TicTacToe />
+            <Watermark />
         </main>
     );
 }
---
+++ src/components/watermark.tsx
@@ -0,0 +1,9 @@
+import { Button } from "./ui/button";
+
+export default function Watermark() {
+    return (
+        <a href="https://eglenn.dev" target="_blank">
+            <Button className="mt-4 shadow-lg">Project by Ethan Glenn</Button>
+        </a>
+    );
+}
